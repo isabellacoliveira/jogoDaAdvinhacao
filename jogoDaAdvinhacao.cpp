b301962640c006b6d627c14b18851434f1a805e2
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-02-14 09:03:54 -0300

    adicionando a estrutura do jogo da advinhação

---
+++ jogoDaAdvinhacao.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main (){ 
+    cout << " ***************************************" << endl;
+    cout << " ** Bem-vindos ao jogo da advinhação! **" << endl;
+    cout << " ***************************************" << endl;
+
+    const int NUMERO_SECRETO = 42;
+    bool nao_acertou = true;
+    int tentativas = 0; 
+
+    // Vamos repetir o progama enquanto o usuario não acertar 
+    while(nao_acertou){
+        tentativas++;
+        int chute;
+        cout << "Tentativas" << tentativas << endl;
+        cout << "Qual o seu chute? ";
+        cin >> chute; 
+        cout << "O valor do chute é " << chute << endl; 
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if(acertou){
+            cout << "Parabéns, você acertou o número secreto!" << endl; 
+            nao_acertou = false;
+        } 
+        else if (maior) {
+            cout << "Seu chute foi maior que o número secreto" << endl; 
+        }
+        else {
+            cout << "Seu chute foi menor que o número secreto" << endl; 
+        }
+        cout << "Fim de jogo!" << endl;
+        cout << "Você acertou o número secreto em: " << tentativas << " tentativas" << endl;
+
+    }
+
+
+}
